f80b8bd62acff6a41f772212bde9abda470dbff3
Ryotaro <mill@Ryotaro-no-MacBook-Air.local>
2018-01-29 02:19:12 +0900

    oops

--- routes/helo.js
+++ routes/helo.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET helo page. */
 router.get('/', function (req, res, next) {
-    res.render('helo', {
-        title: 'Helo',
-        data: {
-            '太郎': 'taro@yamada',
-            '花子': 'hanako@flower',
-            'つやの': 'syoda@tuyano.com'
+    var p1 = req.query.p1;
+    var p2 = req.query.p2;
+    var msg = pl == undefined ? "" : p1 + "," + p2;
+    res.render('helo',
+        {
+            title: 'HELO',
+            msg: msg
         }
-    })
+    );
 })
+
+// router.get('/', function (req, res, next) {
+//     res.render('helo', {
+//         title: 'Helo',
+//         data: {
+//             '太郎': 'taro@yamada',
+//             '花子': 'hanako@flower',
+//             'つやの': 'syoda@tuyano.com'
+//         }
+//     })
+// })
 
 module.exports = router;
