a9e297f76e69bb6db36885d40fbbcef2cdf32e7e
CryptoLab1023 <allscience1023@gmail.com>
2018-04-08 21:31:02 +0900

    API

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
-app.use('/helo', helo);
 
 
 // catch 404 and forward to error handler
