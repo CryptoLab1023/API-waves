9dadceebb44c46536506898c1e7fd702f06dbc12
CryptoLab1023 <allscience1023@gmail.com>
2018-05-11 16:53:04 +0900

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,168 +3,230 @@ const router = express.Router();
 const WavesAPI = require("waves-api");
 const Waves = WavesAPI.create(WavesAPI.TESTNET_CONFIG);
 
-// console.log(Waves.config);
+/*
+console.log(Waves.config);
 
-// const newConfig = {
+const newConfig = {
 
-//   // The byte allowing to distinguish networks (mainnet, testnet, devnet, etc)
-//   networkByte: Waves.constants.TESTNET_BYTE,
+  // The byte allowing to distinguish networks (mainnet, testnet, devnet, etc)
+  networkByte: Waves.constants.TESTNET_BYTE,
 
-//   // Node and Matcher addresses, no comments here
-//   nodeAddress: 'https://testnode2.wavesnodes.com',
-//   matcherAddress: 'https://testnode2.wavesnodes.com/matcher',
+  // Node and Matcher addresses, no comments here
+  nodeAddress: 'https://testnode2.wavesnodes.com',
+  matcherAddress: 'https://testnode2.wavesnodes.com/matcher',
 
-//   // If a seed phrase length falls below that value an error will be thrown
-//   minimumSeedLength: 50
+  // If a seed phrase length falls below that value an error will be thrown
+  minimumSeedLength: 50
 
-// };
+    Waves.config.set(newConfig);
 
-// Waves.config.set(newConfig);
+};
+
+*/
+
+/*
+
 
 const owner = {↲
-    phrase: 'street soup tornado loud pony pact absurd inflict pipe tuna tonight tray manage bargain width',↲
-    address: '3MpqP6XWZ5FtvNBYqi2eUGXwSgp8PZV7Jpw',↲
-    keyPair: ↲ {
-        privateKey: '2ga6KiGd3febVq4bRv9NwdcF1o4ULEQ3aWUnCicKwoWb', ↲
+    phrase: 'street soup tornado loud pony pact absurd inflict pipe tuna tonight tray manage bargain width',
+    ↲
+    address: '3MpqP6XWZ5FtvNBYqi2eUGXwSgp8PZV7Jpw',
+    ↲
+    keyPair: ↲{
+        privateKey: '2ga6KiGd3febVq4bRv9NwdcF1o4ULEQ3aWUnCicKwoWb',
+        ↲
         publicKey: 'BWDQ2bZhMR8a69g1PTXybqiJhp5F4F1UNrTr355gYExS'
     }
 }
+*/
+createSeed = async () => {
+    const seed = Waves.Seed.create();
+    return seed;
+}
+
+returnDecryptedSeed = async (phrase, password) => {
+    const seed = Waves.Seed.decryptSeedPhrase(phrase, password);
+    return seed;
+}
+
+returnEncryptedSeed = async (seed, password) => {
+    const encrypted = seed.encrypt(password);
+    return encrypted;
+}
 
 
-router.get("/getOwner", (req, res) => {
+// 1. CREATING THE VOTER
+// -d [password]
+router.get("/api/createVoter", async (req, res) => {
+    const seed = await createSeed();
+    console.log(seed);
+    const encryptedSeed = await returnEncryptedSeed(seed, req.body.password);
+    console.log(encryptedSeed);
+    res.send(encryptedSeed);
+    // return the encrypted Seed !
+    // necessary to preserve the address and password for voter !!
+})
+
+// 2. CREATEING THE ORACLE
+// -d [password]
+router.get("/api/createOracle", async (req, res) => {
+    const seed = await createSeed();
+    console.log(seed);
+    const encryptedSeed = await returnEncryptedSeed(seed, req.body.password);
+    console.log(encryptedSeed);
+    res.send(encryptedSeed);
+    // return the encrypted Seed !
+    // necessary to preserve the address and password for oracle !!
+})
+
+// 3. DECODE THE SEED IN VOTER
+// -d [encryptedPhrase]
+router.get("/api/getVoterFromEncryptedSeed", async (req, res) => {
+    const seed = getSeedFromEncryptedPhrase(req.body.encryptedPhrase)
+    res.send( returnEncryptedSeed (seed, req.body.password));
+    // validation in db and current account .then(
+    // )
+})
+
+// 3. DECODE THE SEED IN ORACLE
+// -d [encryptedPhrase]
+router.get("/api/getOracleFromEncryptedSeed", async (req, res) => {
+    const seed = getSeedFromEncryptedPhrase(req.body.encryptedPhrase)
+    // validation in db and current account .then(
+    res.send(returnEncryptedSeed(seed, req.body.password));
+    // )
+})
+
+router.get("/api/getOwner", (req, res) => {
     return owner;
 })
 
+
+// 4. CREATE THE TOKEN
 // curl -w '\n' 'http://localhost:8080/job/sample/configSubmit' --data-urlencode 'json={"properties": {"hudson-model-ParametersDefinitionProperty": {"parameterized": {"parameter": {"name": "FileParameter", "description": "Upload file to Jenkins.", "stapler-class": "hudson.model.FileParameterDefinition", "$class": "hudson.model.FileParameterDefinition"}}}}}' -d 'Submit=Save' -XPOST
 // curl -w '\n' 'localhost:3000/issue' -XPOST --data-urlencode 'json={"name": "RyotaroToken", "ammount": 20000000000}'
-router.post("/issue", (req, res) => {
+// {name: , description: , amount: , encryptedPhrase: ,   }
+
+router.post("/api/issue", (req, res) => {
     console.log(req)
     const issueData = {
-
         name: req.body.name,
-        description: 'Some words about it',
-
+        description: req.body.description,
         // With given options you'll have 100000.00000 tokens
         quantity: req.body.amount,
         precision: 5,
-
         // This flag defines whether additional emission is possible
         reissuable: false,
-
         fee: 100000000,
         timestamp: Date.now()
-
     };
     console.log(issueData);
-
-    Waves.API.Node.v1.assets.issue(issueData, owner.keyPair).then((responseData) => {
+    const keyPair = returnDecryptedSeed(req.body.encryptedPhrase).keyPair;
+    // validation in DB whether matched in public address and password;
+    Waves.API.Node.v1.assets.issue(issueData, keyPair).then((responseData) => {
         console.log(responseData);
+        res.send(responseData);
+    }).catch((error) => {
+        res.send(error);
     });
 })
 
+// 5. DISTRIBUTE THE TOKEN
+// the distribution ratio can be changed according to db.
+// {recipientAddress: , assetId: ,amount: , }
 
-router.post("/distribute", (req, res) => {
-  const transferData = {
-    recipient: req.body.recipientAddress,
-    assetId: req.body.assetId,
-    amount: req.body.amount / req.body.addresses.length,
-    feeAssetId: 'WAVES',
-    fee: 100000,
-    attachment: '',
-    timestamp: Date.now()
-  };
-  const keyPair = {
-    privateKey: req.body.privateKey,
-    publicKey: req.body.publicKey,
-  };
-  for (i = 0; i < req.body.addresses; i++){
-    Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
-      console.log(responseData);
-    });
-  }
-})
-
-
-// "voterRecipient=<Address>%22%3a+%7b%22assetId=<>>%22%3a+%7b%22amount=<Number>%22%3a+%7b%22privateKey=<SKey>"
-router.post("/transferFromVoter", (req, res) => {
-  const transferData = {
-        recipient: req.body.voterRecipient, //YesAddress
+router.post("/api/distributeEqually", (req, res) => {
+    const transferData = {
+        recipient: req.body.recipient,
         assetId: req.body.assetId,
         amount: req.body.amount,
         feeAssetId: 'WAVES',
         fee: 100000,
         attachment: '',
         timestamp: Date.now()
-  };
-  const keyPair = {
-    privateKey: req.body.privateKey,
-    publicKey: req.body.publicKey,
-  };
-  Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
-    console.log(responseData);
-  });
+    };
+    console.log(transferData);
+    const keyPair = returnDecryptedSeed(req.body.encryptedPhrase).keyPair;
+    // validation in DB whether matched in public address and password;
+    Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
+        console.log(responseData);
+        res.send(responseData);
+    }).catch((error) => {
+        res.send(error);
+    });
 })
 
-router.post("/transferFromOracle", (req, res) => {
+// 6. TRANSFER FROM VOTER TO YES OR NO ADDRESS THE VENTUARY TOKEN
+// "voterRecipient=<Address>%22%3a+%7b%22assetId=<>>%22%3a+%7b%22amount=<Number>%22%3a+%7b%22privateKey=<SKey>"
+
+router.post("/api/transferFromVoter", (req, res) => {
     const transferData = {
-        recipient: req.body.oracleRecipient,
+        recipient: req.body.recipient, //Yes or NoAddress
         assetId: req.body.assetId,
-        amount: 10000000,
+        amount: req.body.amount,
         feeAssetId: 'WAVES',
         fee: 100000,
         attachment: '',
         timestamp: Date.now()
-  };
-  const keyPair = {
-    privateKey: req.body.privateKey,
-    publicKey: req.body.publicKey,
-  };
-  Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
-    console.log(responseData);
-  });
+    };
+    const keyPair = returnDecryptedSeed(req.body.encryptedPhrase).keyPair;
+    // validation in DB whether matched in public address and password;
+    Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
+        console.log(responseData);
+        res.send(responseData);
+    }).catch((error) => {
+        res.send(error);
+    });
 })
 
 
-router.post("/makeFactorYes", (req, res) => {
-    const seed = Waves.Seed.create();
-    const cancelLeasingData = {
-    transactionId: req.body.transactionId,
-    fee: 100000,
-    timestamp: Date.now()
-  };
-  const keyPair = {
-    privateKey: req.body.privateKey,
-    publicKey: req.body.publicKey,
-  };
-  Waves.API.Node.v1.leasing.cancelLeasing(cancelLeasingData, keyPair).then((responseData) => {
-    console.log(responseData);
-  });
+// 7. TRANSFER FROM ORACLE TO YES OR NO ADDRESS THE VENTUARY TOKEN
+router.post("/api/transferFromOracle", (req, res) => {
+    const transferData = {
+        recipient: req.body.recipient, //Yes or NoAddress
+        assetId: req.body.assetId,
+        amount: req.body.amount,
+        feeAssetId: 'WAVES',
+        fee: 100000,
+        attachment: '',
+        timestamp: Date.now()
+    };
+    const keyPair = returnDecryptedSeed(req.body.encryptedPhrase).keyPair;
+    // validation in DB whether matched in public address and password;
+    Waves.API.Node.v1.assets.transfer(transferData, keyPair).then((responseData) => {
+        console.log(responseData);
+        res.send(responseData);
+    }).catch((error) => {
+        res.send(error);
+    });
 })
 
-router.post("/createAlias", (req, res) => {
-  const createAliasData = {
-    // That's a kind of a nickname you attach to your address
-    alias: req.body.alias,
-    fee: 100000,
-    timestamp: Date.now()
-  };
-  const keyPair = {
-    privateKey: req.body.privateKey,
-    publicKey: req.body.publicKey,
-  };
-  Waves.API.Node.v1.aliases.createAlias(createAliasData, keyPair).then((responseData) => {
-    console.log(responseData);
-  });
+// 8 MAKING YES ADDRESS
+router.post("/api/makeFactorYes", (req, res) => {
+    // if DB has no YES address
+    const seed = createSeed();
+    const encryptedSeed = returnEncryptedSeed(seed, req.body.password)
+    res.send(encryptedSeed);
 })
 
-// curl -sS localhost:3000/getBalance -XGET -d "address=3N9KC7WSaUTw3JS3wPyCgN9qxjK3eEeNWkR"
-router.get("/getBalance", (req, res) => {
+// 9 MAKING NO ADDRESS
+router.post("/api/makeFactorNo", (req, res) => {
+    // if DB has no NO address
+    const seed = createSeed();
+    const encryptedSeed = returnEncryptedSeed(seed, req.body.password)
+    res.send(encryptedSeed);
+})
+
+// curl -sS localhost:3000/api/getBalance -XGET -d "address=3N9KC7WSaUTw3JS3wPyCgN9qxjK3eEeNWkR"
+router.get("/api/getBalance", (req, res) => {
     Waves.API.Node.v1.addresses.balance(req.body.address).then((balance) => {
         console.log(balance);
     });
 });
 
-createAddress = async  () => {
+/*
+
+createAddress = async () => {
     const seed = Waves.Seed.create();
     console.log(seed);
     console.log(seed.phrase); // 'hole law front bottom then mobile fabric under horse drink other member work twenty boss'
@@ -174,8 +236,7 @@ createAddress = async  () => {
 
 createAddress();
 
-
-
+*/
 
 
 
